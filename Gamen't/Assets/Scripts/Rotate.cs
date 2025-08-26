3732de10105a6b47ab6bf963f496148f3cd49c86
Onuram <gsmaruno@gmail.com>
2025-08-25 21:33:49 -0300

    aula 25/08

--- Gamen't/Assets/Scripts/Rotate.cs
+++ Gamen't/Assets/Scripts/Rotate.cs
@@ -5,13 +5,20 @@ public class Rotate : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, 80 * Time.deltaTime, Space.Self);
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Toque");
+        Destroy(this.gameObject);
 
     }
 }
